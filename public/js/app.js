c11e91eee24aad57738ee9bfc9d4eeed33256b32
Lauri Annala <lauri.annala.89@gmail.com>
2016-09-05 16:13:40 +0300

    Add and use angular in app

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,16 @@
 angular.module('app', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            template: '<h1>Main page</h1>'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    })
     .controller('MainCtrl', function() {
         console.log('MainCtrl');
+        var vm = this;
+
+        vm.links = [1,2,3,4,5];
     });
